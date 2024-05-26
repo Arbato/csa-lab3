7279cb4962c78a995a758e5e826d9e25d6770400
kseniya <kseniya.arbatova@gmail.com>
2024-05-26 21:29:49 +0300

    update readme and copied isa+translator from penskoi

---
+++ isa.py
@@ -0,0 +1,67 @@
+import json
+from collections import namedtuple
+from enum import Enum
+
+
+class Opcode(str, Enum):
+    HALT = "halt"
+    HLT = "hlt"
+    NOT = "not"
+    NEG = "neg"
+    JMP = "jmp"
+    JZ = "jz"
+    JNZ = "jnz"
+    LD = "ld"
+    ST = "st"
+    ADD = "add"
+    SUB = "sub"
+    OR = "or"
+    AND = "and"
+    CMP = "cmp"
+
+    def __str__(self):
+        """Переопределение стандартного поведения `__str__` для `Enum`: вместо
+        `Opcode.INC` вернуть `increment`.
+        """
+        return str(self.value)
+
+
+class Term(namedtuple("Term", "line pos symbol")):
+    """Описание выражения из исходного текста программы.
+
+    Сделано через класс, чтобы был docstring.
+    """
+
+
+def write_code(filename, code):
+    """Записать машинный код в файл."""
+    with open(filename, "w", encoding="utf-8") as file:
+        # Почему не: `file.write(json.dumps(code, indent=4))`?
+        # Чтобы одна инструкция была на одну строку.
+        buf = []
+        for instr in code:
+            buf.append(json.dumps(instr))
+        file.write("[" + ",\n ".join(buf) + "]")
+
+
+def read_code(filename):
+    """Прочесть машинный код из файла.
+
+    Так как в файле хранятся не только простейшие типы (`Opcode`, `Term`), мы
+    также выполняем конвертацию в объекты классов вручную (возможно, следует
+    переписать через `JSONDecoder`, но это скорее усложнит код).
+
+    """
+    with open(filename, encoding="utf-8") as file:
+        code = json.loads(file.read())
+
+    for instr in code:
+        # Конвертация строки в Opcode
+        instr["opcode"] = Opcode(instr["opcode"])
+
+        # Конвертация списка term в класс Term
+        if "term" in instr:
+            assert len(instr["term"]) == 3
+            instr["term"] = Term(instr["term"][0], instr["term"][1], instr["term"][2])
+
+    return code
