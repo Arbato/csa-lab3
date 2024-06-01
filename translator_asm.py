e6cfaacbf1f61ca1475cd338c1a5abacb6fc25eb
Arbato <kseniya.arbatova@gmail.com>
2024-06-01 23:18:05 +0300

    translator add func

--- translator_asm.py
+++ translator_asm.py
@@ -27,23 +27,63 @@ def section_split(source):
     print(data, code)
     return data, code
 
-def translate_stage_1_data(data: list):
 
 
+
+def translate_stage_1(text):
+    code = []
+    for line_num, raw_line in enumerate(text, 1):
+        token = get_meaningful_token(raw_line)
+        if token == "":
+            continue
+
+        pc = len(code)
+        if token.endswith(":"):  # токен содержит метку
+            label = token.strip(":")
+            assert label not in labels, "Redefinition of label: {}".format(label)
+            labels[label] = pc
+        elif " " in token:  # токен содержит инструкцию с операндом (отделены пробелом)
+            sub_tokens = token.split(" ")
+            assert len(sub_tokens) == 2, "Invalid instruction: {}".format(token)
+            mnemonic, arg = sub_tokens
+            opcode = Opcode(mnemonic)
+            assert(opcode in Opcode.opcodes_with_arg) #  "instructions take an argument"
+            code.append({"index": pc, "opcode": opcode, "arg": arg, "term": Term(line_num, 0, token)})
+        else:  # токен содержит инструкцию без операндов
+            opcode = Opcode(token)
+            code.append({"index": pc, "opcode": opcode, "term": Term(line_num, 0, token)})
+
+    return labels, code
+
+
+def translate_stage_2(labels, code):
+    """Второй проход транслятора. В уже определённые инструкции подставляются
+    адреса меток."""
+    for instruction in code:
+        if "arg" in instruction:
+            label = instruction["arg"]
+            assert label in labels, "Label not defined: " + label
+            instruction["arg"] = labels[label]
+    return code
+
+def data_translate_stage_1(text):
+    
 
 
 def translate():
     data, code = section_split(source)
-    
+    data = 
+    full_code = data+code
+    return full_code
 
 
 def main(source: str, target: str):
     """Функция запуска транслятора. Параметры -- исходный и целевой файлы."""
     with open(source, encoding="utf-8") as f:
-        source = f.read().split("\n")
+        lines = f.read().split("\n")
 
-    source = trimmer(source)
-    
+    lines = trimmer(lines)
+    code = translate(lines)
 
     write_code(target, code)
     print("source LoC:", len(source), "code instr:", len(code))
