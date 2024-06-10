196dbe807fab17c346b315fff8349d3019a4f587
kseniya <kseniya.arbatova@gmail.com>
2024-06-10 14:20:22 +0300

    finished translator

--- translator_asm.py
+++ translator_asm.py
@@ -4,6 +4,14 @@ from isa import Opcode, Term, write_code
 
 labels = {}
 
+
+def get_meaningful_token(line):
+    """Извлекаем из строки содержательный токен (метка или инструкция), удаляем
+    комментарии и пробелы в начале/конце строки.
+    """
+    return line.split(";", 1)[0].strip()
+
+
 def trimmer(code):
     result = []
     for line in code:
@@ -27,17 +35,14 @@ def section_split(source):
     print(data, code)
     return data, code
 
-
-
-
-def translate_stage_1(text):
+def translate_stage_1(data, text):
     code = []
     for line_num, raw_line in enumerate(text, 1):
         token = get_meaningful_token(raw_line)
         if token == "":
             continue
 
-        pc = len(code)
+        pc = len(code)+len(data)
         if token.endswith(":"):  # токен содержит метку
             label = token.strip(":")
             assert label not in labels, "Redefinition of label: {}".format(label)
@@ -53,10 +58,9 @@ def translate_stage_1(text):
             opcode = Opcode(token)
             code.append({"index": pc, "opcode": opcode, "term": Term(line_num, 0, token)})
 
-    return labels, code
+    return code
 
-
-def translate_stage_2(labels, code):
+def translate_stage_2(labels, code: list):
     """Второй проход транслятора. В уже определённые инструкции подставляются
     адреса меток."""
     for instruction in code:
@@ -66,25 +70,92 @@ def translate_stage_2(labels, code):
             instruction["arg"] = labels[label]
     return code
 
+
+
+
 def data_translate_stage_1(text):
+    """
+    converts data that looks like
+    ["int: 123",
+    "msg: "hi""
+    ]
+    to 
+    [{index 0, opcode: "jmp",  arg: 5 }, 
+     {index 1, arg: 123 }, 
+     {index 2, arg: 2},
+     {index 3, arg: "h"},
+     {index 4, arg: "i"}
+     ]"""
+    
+    code = []
+    for line_num, raw_line in enumerate(text, 1):
+        token = get_meaningful_token(raw_line)
+        if token == "":
+            continue
+
+        pc = len(code)+1
+        if ":" in token:
+            if "\"" in token:  # data is a string
+                sub_tokens  = token.split(": ")
+                assert len(sub_tokens) == 2, "Invalid data entry: {}".format(token)
+
+                label, string = sub_tokens
+                assert label not in labels, "Redefinition of label: {}".format(label)
+                labels[label] = pc
+                string = string.strip("\"")
+                length = len(string)
+                code.append({"index": pc, "arg": length})
+                for letter in string:
+                    pc = len(code)+1
+                    code.append({"index": pc, "arg": letter })
+        
+        
+            elif " " in token:  # data is an int
+                sub_tokens = token.split(": ")
+                assert len(sub_tokens) == 2, "Invalid instruction: {}".format(token)
+                label, arg = sub_tokens
+                labels[label] = pc
+
+                code.append({"index": pc, "arg": arg})
+            print(token, code,  "\n")
+
+
+
+    code.insert(0, {"index": 0, "opcode": "jmp",  "arg": len(code)+1 })
+
+    print(labels, code,  "\n")
+    return code
     
 
 
-def translate():
+def translate(source: list):
     data, code = section_split(source)
-    data = 
-    full_code = data+code
-    return full_code
+    print("section splity data+code")
+    print (data)
+    print(code)
+    data = data_translate_stage_1(data)
+    code = translate_stage_1(data, code)
+
+    print("data+code after 1 stage")
+    print(data )
+    print(code)
+
+    print()
+    print("labels")
+    print (labels)
+
+
+    full_code = translate_stage_2(labels, code)
+    return data+full_code
 
 
 def main(source: str, target: str):
     """Функция запуска транслятора. Параметры -- исходный и целевой файлы."""
     with open(source, encoding="utf-8") as f:
         lines = f.read().split("\n")
-
     lines = trimmer(lines)
     code = translate(lines)
-
+    print("code in main ", code, "\n")
     write_code(target, code)
     print("source LoC:", len(source), "code instr:", len(code))
 
@@ -93,3 +164,7 @@ if __name__ == "__main__":
     assert len(sys.argv) == 3, "Wrong arguments: translator_asm.py <input_file> <target_file>"
     _, source, target = sys.argv
     main(source, target)
+
+
+
+   
